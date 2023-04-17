ff0a5765f2485c3c5a0d2583cffda9ab28677e6d
ZhagoAlvarez <123136234+ZhagoAlvarez@users.noreply.github.com>
2023-04-16 22:22:17 -0600

    Domingo 16
    Domingo 16

---
+++ js/register.js
@@ -0,0 +1,25 @@
+function registrar(){
+        var correoElectrónico = document.querySelector("#InputEmail").value;
+        var nombre = document.querySelector("#InputName").value;
+        var fechaNacimiento =  document.querySelector('input[type="date"]').value;
+        var hoy = new Date();
+        var cumpleaños  = new Date(fechaNacimiento);
+        var edad = hoy.getFullYear() - cumpleaños.getFullYear();
+        var mes = hoy.getMonth() - cumpleaños.getMonth();
+        var Output = "";
+        if (mes < 0 || (mes === 0 && hoy.getDate() < cumpleaños.getDate())) {
+            edad--;
+        }
+        
+        Output = 
+          "Señor(a): "+nombre+" por favor compruebe a continuación sus datos: "+"\n"+
+          "Correo Electrónico: "+correoElectrónico+"\n"+
+          "Edad: "+edad+"\n"+
+          " "+"\n"+
+          "Si los datos están completos y correctos lo contactaremos lo mas pronto posible. Gracias por confiar en nuestro servicio."
+          ;
+
+     alert(Output);
+  }
+  
+  
